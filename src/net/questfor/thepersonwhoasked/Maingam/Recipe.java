68ec20b154203e3a8e27f2b8f482a842facfdd53
Bruhkid2345 <83349478+Bruhkid2345@users.noreply.github.com>
2022-06-13 21:45:31 -0400

    Delete src directory

--- src/net/questfor/thepersonwhoasked/Maingam/Recipe.java
+++
@@ -1,8 +0,0 @@
-package net.questfor.thepersonwhoasked.Maingam;
-
-import net.questfor.thepersonwhoasked.entities.LivingEntity;
-public class Recipe extends Data{
-    public int Type = 0; //1 for crafting, 2 for brewing
-    public LivingEntity[] Recipe = new LivingEntity[9];
-    public LivingEntity Result;
-}
