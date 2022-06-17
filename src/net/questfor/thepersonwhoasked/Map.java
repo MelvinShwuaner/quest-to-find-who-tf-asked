2585bd37192fc992526fa28eff44c1cd995da18c
Bruhkid2345 <83349478+Bruhkid2345@users.noreply.github.com>
2022-06-16 20:55:44 -0400

    Delete src directory

--- src/net/questfor/thepersonwhoasked/Map.java
+++
@@ -1,8 +0,0 @@
-package net.questfor.thepersonwhoasked;
-
-import java.io.Serializable;
-
-public class Map implements Serializable {
-    private static final long serialVersionUID = -694201445L;
-    public int[][][] mapRendererID;
-}
